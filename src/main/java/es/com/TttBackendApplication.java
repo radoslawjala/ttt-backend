e9a9cbb43961ca1420a4903a80b9ef14c1a7a584
radek <marion.cobretti@gmail.com>
2021-05-30 18:16:38 +0200

    Initial commit

---
+++ src/main/java/es/com/TttBackendApplication.java
@@ -0,0 +1,13 @@
+package es.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TttBackendApplication.class, args);
+    }
+
+}
